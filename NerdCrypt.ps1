f4b1ca53314fecd8e7ec85870d0d099ae87ad099
Alain Herve <alain.1337dev@outlook.com>
2022-08-23 12:55:52 +0200

    Update NerdCrypt.ps1

--- NerdCrypt.ps1
+++ NerdCrypt.ps1
@@ -260,6 +260,16 @@ class xgen {
 [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingInvokeExpression", '')]
 class XConvert {
     XConvert() {}
+    [PSObject[]]Static XmlToPSObject([xml]$XML) {
+        $Out = @(); foreach ($Object in @($XML.Objects.Object)) {
+            $PSObject = New-Object PSObject
+            foreach ($Property in @($Object.Property)) {
+                $PSObject | Add-Member NoteProperty $Property.Name $Property.InnerText
+            }
+            $Out += $PSObject
+        }
+        return $Out
+    }
     [string]static SecurestringToString([System.Security.SecureString]$SecureString) {
         [string]$Pstr = [string]::Empty;
         [IntPtr]$zero = [IntPtr]::Zero;
