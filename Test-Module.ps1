700d589e7c3b8c4fbfaf7fdec30cfb0509012922
Alain Herve <alain.1337dev@outlook.com>
2022-09-06 17:49:42 +0200

    Create Test-Module.ps1

---
+++ Test-Module.ps1
@@ -0,0 +1,35 @@
+﻿<#	
+	.NOTES
+	===========================================================================
+	 Created on:   	9/6/2022 5:36 PM
+	 Created by:   	alain
+	 Organization: 	
+	 Filename:     	Test-Module.ps1
+	===========================================================================
+	.DESCRIPTION
+	The Test-Module.ps1 script lets you test the functions and other features of
+	your module in your PowerShell Studio module project. It's part of your project,
+	but it is not included in your module.
+
+	In this test script, import the module (be careful to import the correct version)
+	and write commands that test the module features. You can include Pester
+	tests, too.
+
+	To run the script, click Run or Run in Console. Or, when working on any file
+	in the project, click Home\Run or Home\Run in Console, or in the Project pane, 
+	right-click the project name, and then click Run Project.
+#>
+
+
+#Explicitly import the module for testing
+Import-Module 'NerdCrypt'
+
+#Run each module function
+Write-HelloWorld
+
+#Sample Pester Test
+#Describe "Test NerdCrypt" {
+#	It "tests Write-HellowWorld" {
+#		Write-HelloWorld | Should BeExactly "Hello World"
+#	}	
+#}
