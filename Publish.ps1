d6eefc2ac0e461aaabd9306b89612949679884fb
Alain Herve <alain.1337dev@outlook.com>
2022-09-08 22:26:03 +0200

    Update Publish.ps1

--- Publish.ps1
+++ Publish.ps1
@@ -1,8 +1,7 @@
 ﻿Rename-Item "$PSScriptRoot\Module" -NewName "Nerdcrypt"
 if ($ENV:COMPUTERNAME -eq 'CODETRON' -and [string]::IsNullOrEmpty($Env:GITHUB_ACTION_PATH)) {
     if (![regex]::IsMatch($Env:NUGETAPIKEY, "^[A-Fa-f0-9]{600,700}$")) {
-        $secure = ConvertTo-SecureString (Read-Host -AsSecureString -Prompt 'Enter your Nuget APIKEY')
-        $export = $secure | ConvertFrom-SecureString
+        $export = (Read-Host -AsSecureString -Prompt 'Enter your Nuget APIKEY') | ConvertFrom-SecureString
         Add-EnvironmentVariable -Name NUGETAPIKEY -Value $export
     }
     $APIKEY = $(New-Object system.Management.Automation.PSCredential("test", $(ConvertTo-SecureString $Env:NUGETAPIKEY))).GetNetworkCredential().Password
