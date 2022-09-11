7e2ae4cafcfe61a5cc8d79eaf8d784e48bba129a
Alain Herve <alain.1337dev@outlook.com>
2022-09-11 18:02:56 +0200

    Update Test-Module.ps1

--- Test-Module.ps1
+++ Test-Module.ps1
@@ -3,8 +3,7 @@
     Run Tests
 .DESCRIPTION
 	The Test-Module.ps1 script lets you test the functions and other features of
-	your module in your PowerShell Studio module project. It's part of the project,
-	but it is not included in the module.
+	your module. It's part of the project, but it is not included in the module.
 .NOTES
 	===========================================================================
 	Created on:   	9/6/2022 5:36 PM
@@ -13,10 +12,19 @@
 	Filename:     	Test-Module.ps1
 	===========================================================================
 #>
-
-#Explicitly import the module for testing
-$manifestPath = (Join-Path $module "NerdCrypt.psd1")
-Import-Module $manifestPath
+param (
+    [Parameter(Mandatory = $true, Position = 0)]
+    [Alias('Module')][string]$ModulePath,
+    # Path Containing Tests
+    [Parameter(Mandatory = $true, Position = 1)]
+    [Alias('Tests')][string]$TestsPath
+)
+$manifestPath = Join-Path $ModulePath "NerdCrypt.psd1"
+if ([IO.File]::Exists($manifestPath)) {
+    Import-Module $manifestPath
+} else {
+    throw 'Module manifest file Was not Found!'
+}
 Write-Verbose "[+] Running tests ..."
-Test-ModuleManifest -Path $manifestPath -ErrorAction Stop
-Invoke-Pester -Path "$PSScriptRoot\tests" -OutputFormat NUnitXml -OutputFile Tests\results.xml
+Test-ModuleManifest -Path $manifestPath -ErrorAction Stop -Verbose
+Invoke-Pester -Path $TestsPath -OutputFormat NUnitXml -OutputFile "$TestsPath\results.xml"
