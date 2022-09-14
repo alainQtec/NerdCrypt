c0c4b9fce44d227b4b0caac93240d50c89623a60
Alain Herve <alain.1337dev@outlook.com>
2022-09-14 15:27:45 +0200

    Create Install-VSTS.ps1

---
+++ Private/Install-VSTS.ps1
@@ -0,0 +1,29 @@
+﻿# https://docs.microsoft.com/en-us/cli/vsts/install?view=vsts-cli-latest
+# https://aka.ms/vsts-cli-windows-installer
+function Install-VSTS {
+    <#
+    .SYNOPSIS
+        Visual Studio Team Services and Team Foundation Server.
+    .DESCRIPTION
+        Visual Studio Application Lifecycle Management.
+    .NOTES
+        Information or caveats about the function e.g. 'This function is not supported in Linux'
+    .LINK
+        Specify a URI to a help page, this will show when Get-Help -Online is used.
+    .EXAMPLE
+        Test-MyTestFunction -Verbose
+        Explanation of the function or its result. You can include multiple examples with additional .EXAMPLE lines
+    #>
+    [CmdletBinding()]
+    param (
+    )
+
+    begin {
+    }
+
+    process {
+    }
+
+    end {
+    }
+}
