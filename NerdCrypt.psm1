4030295c6eb7b052fa747b846d598c8e878e14ba
Alain Herve <alain.1337dev@outlook.com>
2022-12-13 08:04:34 +0200

    Update NerdCrypt.psm1

--- NerdCrypt.psm1
+++ NerdCrypt.psm1
@@ -1,7 +1,10 @@
 $Private = Get-ChildItem ([IO.Path]::Combine($PSScriptRoot, 'Private')) -Filter "*.ps1" -ErrorAction SilentlyContinue
 $Public = Get-ChildItem ([IO.Path]::Combine($PSScriptRoot, 'Public')) -Filter "*.ps1" -ErrorAction SilentlyContinue
+
 # Load dependencies
 $PrivateModules = [string[]](Get-ChildItem ([IO.Path]::Combine($PSScriptRoot, 'Private')) -ErrorAction SilentlyContinue | Where-Object { $_.PSIsContainer } | Select-Object -ExpandProperty FullName)
+
+# Import Private modules
 if ($PrivateModules.Count -gt 0) {
     foreach ($Module in $PrivateModules) {
         Try {
