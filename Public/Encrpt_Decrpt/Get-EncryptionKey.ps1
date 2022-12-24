22dd182207dce0a7a8c83c55d055115d4a7992a0
Alain Herve <alain.1337dev@outlook.com>
2022-12-24 07:47:26 +0200

    Create Get-EncryptionKey.ps1

---
+++ Public/Encrpt_Decrpt/Get-EncryptionKey.ps1
@@ -0,0 +1,28 @@
+﻿function Get-EncryptionKey {
+    <#
+    .SYNOPSIS
+        This function could be used to retrieve the current encryption key being used by the module.
+    .DESCRIPTION
+        A longer description of the function, its purpose, common use cases, etc.
+    .NOTES
+        Information or caveats about the function e.g. 'This function is not supported in Linux'
+    .LINK
+        https://github.com/alainQtec/NerdCrypt/blob/main/Public/Encrpt_Decrpt/Get-EncryptionKey.ps1
+    .EXAMPLE
+        Test-MyTestFunction -Verbose
+        Explanation of the function or its result. You can include multiple examples with additional .EXAMPLE lines
+    #>
+    [CmdletBinding()]
+    param (
+
+    )
+
+    begin {
+    }
+
+    process {
+    }
+
+    end {
+    }
+}
