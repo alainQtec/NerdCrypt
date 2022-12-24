2b634114c714ea2d96e95a0f0ea2802f2ef5e54e
Alain Herve <alain.1337dev@outlook.com>
2022-12-24 07:47:22 +0200

    Create Decrypt-Data.ps1

---
+++ Public/Encrpt_Decrpt/Decrypt-Data.ps1
@@ -0,0 +1,29 @@
+﻿function Decrypt-Data {
+    <#
+    .SYNOPSIS
+        This function is used to decrypt a piece of data that has been previously encrypted.
+    .DESCRIPTION
+        A longer description of the function, its purpose, common use cases, etc.
+    .NOTES
+        Information or caveats about the function e.g. 'This function is not supported in Linux'
+    .LINK
+        https://github.com/alainQtec/NerdCrypt/blob/main/Public/Encrpt_Decrpt/Decrypt-Data.ps1
+    .EXAMPLE
+        Decrypt-Data -data
+        Explanation of the function or its result. You can include multiple examples with additional .EXAMPLE lines
+    #>
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseApprovedVerbs', '', Justification = 'Prefer verb usage')]
+    [CmdletBinding()]
+    param (
+
+    )
+
+    begin {
+    }
+
+    process {
+    }
+
+    end {
+    }
+}
